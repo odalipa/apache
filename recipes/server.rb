0a3c21236ddc18f7fdf7dcd1b1a7bf865a9f6f57
Mariano Apilado <miapilado@yahoo.com>
2017-10-31 00:08:08 +0000

    Apache Server Chef Configuration

---
+++ recipes/server.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, workd!</h1>'
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
